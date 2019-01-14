bbb69130eb6e85763a339ea0a6e840493683c183
Faris Widyantho <faris.widyantho@orori.com>
2019-01-14 10:07:48 +0700

    fixed

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -64,7 +64,7 @@ public class SplashScreen {
                     //                 });
                     //             }
                     //         }
-                        }).start();
+                    //     }).start();
                     }
                 }
             }
